aea5bac4b927625511347dbdea4ea3bb60727e0f
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-01-26 23:16:52 +0545

    Tailwind css #1

---
+++ src/mainNavigation/Botton.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Botton = () => {
+    return (
+        <View>
+            <Text>Botton</Text>
+        </View>
+    )
+}
+
+export default Botton
