edb346e06fc5c8cc7dbd0c9a7c0caa49e48e3390
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-01-27 18:27:00 +0545

    Packaged Patched #5

---
+++ src/screens/EntryPoint.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EntryPoint = () => {
+  return (
+    <View>
+      <Text>EntryPoint</Text>
+    </View>
+  )
+}
+
+export default EntryPoint
---
+++ src/screens/Home/Suggested.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Suggested = () => {
+  return (
+    <View>
+      <Text>Suggested</Text>
+    </View>
+  )
+}
+
+export default Suggested
