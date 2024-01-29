db850146d56106e608bf0f4b8295cbcba0067157
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-01-29 19:36:24 +0545

    skeleton started #15

---
+++ src/components/skeleton/SuggestedSkeleton.tsx
@@ -0,0 +1,25 @@
+import { View, Text, FlatList } from 'react-native';
+import React from 'react'
+
+const SuggestedSkeleton = () => {
+    const skeletonNumber = [1, 2, 3]
+    return (
+        <View className=' h-auto w-full'>
+            <View className='h-48 w-full  flex items-center flex-row justify-evenly'>
+                {skeletonNumber.map((current, index) => {
+                    return (
+                        <View key={current}  className='w-32  h-full flex items-center justify-evenly   '>
+                            <View role='status' className='bg-gray-700 animate-pulse h-36 w-32  rounded-3xl overflow-hidden'>
+
+                            </View>
+                            <View className=' w-full bg-gray-700 rounded-md mt-1 h-9 flex items-center justify-center'>
+                            </View>
+                        </View>
+                    )
+                })}
+            </View>
+        </View>
+    )
+}
+
+export default SuggestedSkeleton
