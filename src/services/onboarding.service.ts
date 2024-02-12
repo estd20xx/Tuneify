e0bd8941b2672ef57ff4a8a21ef70e0e22e48d42
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-02-12 14:45:14 +0545

    fix: unexpected app crashing #63

---
+++ src/services/onboarding.service.ts
@@ -0,0 +1,16 @@
+import { Ionboard } from "../Interfaces/onboard.interface"
+import { onBoardingScreen } from "../Types/Types"
+export default class OnboadringService implements Ionboard {
+    constructor(private api: string) { }
+    public getOnboardImage = (): string => {
+        return this.api
+    }
+    public onboardHandler = (setnre: (newNre: number) => void, initial: number, navigation: onBoardingScreen) => {
+        if (initial == 2) {
+            navigation.navigate("bottom")
+            setnre(0)
+            return
+        }
+        setnre(initial + 1)
+    }
+}
