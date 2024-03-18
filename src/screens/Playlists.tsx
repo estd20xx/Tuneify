cdf8144e6bc293eec9736e6da9094bc0e0aeff6e
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-03-19 00:16:54 +0545

    'Cache'

--- src/screens/Playlists.tsx
+++ src/screens/Playlists.tsx
@@ -1,13 +1,12 @@
-import {Text, View} from "react-native"
+import {View, Text} from "react-native"
 import React from "react"
 
 const Playlists = () => {
   return (
-    <View className="w-full h-screen flex items-center justify-center">
-      <Text className="text-white text-xl font-[100] tracking-widest">
-        Playlist
-      </Text>
+    <View>
+      <Text>Playlists</Text>
     </View>
   )
 }
+
 export default Playlists
