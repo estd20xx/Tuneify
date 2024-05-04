f5320e8bc12101b007a047921445fd9ccb469a6f
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-05-04 22:33:09 +0545

    'fix: code-refactor #156'

--- src/Interfaces/Suggested.interface.ts
+++ src/Interfaces/Suggested.interface.ts
@@ -1,27 +1,28 @@
-import { ChartsTypes } from "../Types/Types"
-import { Tr, TrendingAlbumTypes, AlbumTypes } from "./album.interface"
-import { TrendingSongTypes } from "./songs.interface"
-import { PlaylistTypes } from "./playlist.interface"
+import { SongsTypes } from "./songs.interface"
+import {
+  AlbumResponse,
+  ChartsResponse,
+  PlaylistResponse,
+} from "../api/interface/module.interface"
 export interface SuggestedDataInterfaceChild {
-  albums: AlbumTypes[]
-  playlists: PlaylistTypes[]
-  charts: ChartsTypes[]
-  trending: Tr
+  albums: AlbumResponse[]
+  playlists: PlaylistResponse[]
+  charts: ChartsResponse[]
+  trending: any // TODO  Needs to be optimize
 }
 export interface Http {
   data: SuggestedDataInterfaceChild
 }
-
 export default interface Isuggested {
   wait: (timeout: number) => Promise<void>
   randomGenerator: (initial: number, final: number) => number
   http: (url: string) => Promise<Http>
   getSuggestedData: (
-    setAlbums: (albums: AlbumTypes[]) => void,
-    setPlst: (playlist: PlaylistTypes[]) => void,
-    setChst: (charts: ChartsTypes[]) => void,
+    setAlbums: (albums: AlbumResponse[]) => void,
+    setPlst: (playlist: PlaylistResponse[]) => void,
+    setChst: (charts: ChartsResponse[]) => void,
     setLd: (ld: boolean) => void,
-    setTrndSong: (trndSong: TrendingSongTypes[]) => void,
-    setTrndAlb: (trndAlbum: TrendingAlbumTypes[]) => void
+    setTrndSong: (trndSong: SongsTypes[]) => void,
+    setTrndAlb: (trndAlbum: AlbumResponse[]) => void
   ) => void
 }
