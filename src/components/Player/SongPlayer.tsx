e74411608c1662b5d497b3975dea747b8e7f35a4
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2024-05-18 18:33:05 +0545

    slices

--- src/components/Player/SongPlayer.tsx
+++ src/components/Player/SongPlayer.tsx
@@ -25,10 +25,7 @@ import { lyricsApi } from "../../api/api"
 const service = new TuneifyService(lyricsApi)
 import { addUserFavouritesData } from "../../store/slices/favourite.slice"
 import { TypedSelectorHook, useAppDispatch } from "../../hooks/store.hook"
-import {
-  changeTunifyState,
-  tunifyChild,
-} from "../../store/slices/childState.slice"
+import { changeTunifyState, tunifyChild } from "../../store/slices/childState.slice"
 import Messanger from "../message/Message"
 const SongPlayer = ({
   isVisible,
@@ -157,7 +154,7 @@ const SongPlayer = ({
             <View className="relative h-1/2 w-full flex items-center justify-center  ">
               <Animated.View
                 style={[frontAnimatedStyle, { backfaceVisibility: "hidden" }]}
-                className=" w-[90%]  h-96   rounded-3xl overflow-hidden bg-red-500"
+                className=" w-[90%]  h-96   rounded-3xl overflow-hidden"
               >
                 <TrackImage
                   source={{
--- src/store/slices/album.slice.ts
+++ src/store/slices/album.slice.ts
@@ -2,7 +2,6 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 import { AlbumDetailsResponse } from "../../api/interface/album.interface"
 import { album } from "../actions/album.action"
 import { RootState } from "../store"
-
 interface InitialAlbumSongsInterface {
   data: AlbumDetailsResponse | null
   isLoading: boolean
@@ -18,29 +17,14 @@ const albumSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(
-        album.getAlbumSongs.pending,
-        (state: InitialAlbumSongsInterface) => {
-          state.isLoading = true
-        }
-      )
-      .addCase(
-        album.getAlbumSongs.fulfilled,
-        (
-          state: InitialAlbumSongsInterface,
-          action: PayloadAction<AlbumDetailsResponse>
-        ) => {
-          state.data = action.payload
-          state.isLoading = false
-        }
-      )
-      .addCase(
-        album.getAlbumSongs.rejected,
-        (state: InitialAlbumSongsInterface) => {
-          state.isError = true
-        }
-      )
+    builder.addCase(album.getAlbumSongs.pending, (state: InitialAlbumSongsInterface) => {
+      state.isLoading = true
+    }).addCase(album.getAlbumSongs.fulfilled, (state: InitialAlbumSongsInterface, action: PayloadAction<AlbumDetailsResponse>) => {
+      state.data = action.payload
+      state.isLoading = false
+    }).addCase(album.getAlbumSongs.rejected, (state: InitialAlbumSongsInterface) => {
+      state.isError = true
+    })
   },
 })
 export const albumData = (state: RootState) => state.persistedReducer.album
