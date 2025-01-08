7417520433600b41e3f1afc93f50ac84e2b483aa
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2025-01-08 21:53:14 +0545

    'code refactor'

--- src/Interfaces/tuneifySetUp.interface.ts
+++ src/Interfaces/tuneifySetUp.interface.ts
@@ -1,7 +1,7 @@
 import { Dispatch, UnknownAction } from "@reduxjs/toolkit"
 import { Event, PlaybackState, Track } from "react-native-track-player"
 import { InitialCentralQueue } from "../store/slices/new/Queue.slice"
-import { InitialSongStateTypes } from "./tuneifySlice.interface"
+import { StoreSongTypes } from "./tuneifySlice.interface"
 export interface ITuneify {
   getLyrics: (setlyrics: (lyric: string) => void) => Promise<void>
   repeatMode: (state: InitialCentralQueue, dispatch: Dispatch<UnknownAction>) => Promise<void>
@@ -17,6 +17,6 @@ export interface ITuneify {
     dispatch: Dispatch<UnknownAction>
   ) => Promise<void>
   getEvent: () => Event[]
-  setUpPlayer: (data: InitialSongStateTypes) => void
+  setUpPlayer: (data: Array<StoreSongTypes>) => void
   handleBottomCondition: (setCurrentTrack: (track: Track) => void) => Promise<void>
 }
