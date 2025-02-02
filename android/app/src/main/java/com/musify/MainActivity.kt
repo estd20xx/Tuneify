d9bb0953e1624ebf4bad462e2c7354d7ac47605c
Sushant khadka <87481819+NamelesssNerd@users.noreply.github.com>
2025-02-02 18:25:16 +0545

    code re-factor

--- android/app/src/main/java/com/musify/MainActivity.kt
+++ android/app/src/main/java/com/musify/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.musify
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.Settings
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,11 +8,11 @@ import org.devio.rn.splashscreen.SplashScreen
 
 
 class MainActivity : ReactActivity() {
-  override fun getMainComponentName(): String = "Musify"
-  override fun onCreate(savedInstanceState: Bundle?) {
-    SplashScreen.show(this);
-    super.onCreate(savedInstanceState)
-  }
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-  DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun getMainComponentName(): String = "Musify"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState)
+    }
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
